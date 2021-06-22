3652b6ed4ebd96d7acd74a9bb9f2c8c789b77dcf
atharva23 <atharva.sayankar@gmail.com>
2021-06-22 12:49:25 +0530

    added

--- src/exam/ConvertToBinary.java
+++ src/exam/ConvertToBinary.java
@@ -1,20 +1,26 @@
 package exam;
-
+import java.util.Scanner;
 public class ConvertToBinary {
 
-  public String toBinary(final int n) {
+  public static String toBinary(int n) {
 
     int binary[] = new int[40];
     int index = 0;
-    while(decimal > 0){
-      binary[index++] = decimal%2;
-      decimal = decimal/2;
+    while(n > 0){
+      binary[index++] = n%2;
+      n = n/2;
     }
     for(int i = index-1;i >= 0;i--){
       System.out.print(binary[i]);
     }
-    System.out.println();//new line  
+    System.out.println();//new line
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
     return "";
+  }
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    System.out.println(toBinary(n));
+
   }
 }
--- src/exam/ShiftElementsInArray.java
+++ src/exam/ShiftElementsInArray.java
@@ -1,10 +1,33 @@
 package exam;
-
+import java.util.Arrays;
+import java.util.Scanner;
 public class ShiftElementsInArray {
 
-  public void shift(int[] arr) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-
+  static void rotate(int[] arr)
+  {
+    int x = arr[arr.length-1], i;
+    for (i = arr.length-1; i > 0; i--)
+      arr[i] = arr[i-1];
+    arr[0] = x;
   }
+
+    public static void main(String[] args)
+    {
+      Scanner sc = new Scanner(System.in);
+      int n=sc.nextInt();
+      int[] arr = new int[n];
+      for(int i=0; i<n; i++)
+      {
+        arr[i]=sc.nextInt();
+      }
+      System.out.println(Arrays.toString(arr));
+
+      rotate(arr);
+
+      System.out.println("Rotated Array is");
+      System.out.println(Arrays.toString(arr));
+    }
+
+
 
 }
